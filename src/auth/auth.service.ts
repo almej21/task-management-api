edf0bc38bb781910ca49cbda64b82f49eb2db3b3
almej21 <almogaharon21@gmail.com>
2023-08-11 18:41:31 +0300

    Initial commit, working api with only user sign up and RefToken and AccTok

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import { Tokens } from './types';
+const prisma = new PrismaClient()
+
+
+@Injectable()
+export class AuthService {
+    constructor(private prisma: PrismaService, private jwtService: JwtService) { 
+    }
+
+    async signup(dto: AuthDto): Promise<Tokens>  {
+        // hashing the password  
+        const hash = await this.hashedData(dto.password);
+
+        // creating a new user
+        const newUser = await this.prisma.user.create({ 
+            data: {
+                email: dto.email,
+                hash: hash,
+                },
+             });
+
+        // getting new tokens based on the id and email.
+        const tokens = await this.getTokens(newUser.id, newUser.email);
+        // updating the user's tokens.
+        await this.updateRtHash(newUser.id, tokens.refresh_token);
+        return tokens;
+    }
+
+    signin() {}
+    logout() {}
+    refreshTokens() {}
+
+    hashedData(data: string) {
+        return bcrypt.hash(data, 10);
+    }
+
+    async getTokens(userId: number, email: string) {
+        const [at, rt] = await Promise.all([
+            this.jwtService.signAsync({
+                sub: userId,
+                email,
+            }, { secret:'at-secret',expiresIn: 60 * 15}),
+            this.jwtService.signAsync({
+                sub: userId,
+                email,
+            }, { secret:'rt-secret', expiresIn: 60 * 60 * 24})
+        ])
+        
+        return {
+            access_token: at,
+            refresh_token: rt,
+        }
+    }
+
+    async updateRtHash(userId: number, rt: string) {
+        const hash = await this.hashedData(rt);
+        await this.prisma.user.update({
+            where: {
+                id: userId,
+            },
+            data: {
+                hashedRT: hash,
+            }
+        })
+    }
+}
